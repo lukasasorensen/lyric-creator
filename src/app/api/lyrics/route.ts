187cde71965c25ea10d619301d9d3f4250bddd1a
Lukas Sorensen <lsorensen@sportsdigita.com>
2024-07-22 17:58:12 -0500

    cleanup

--- src/app/api/lyrics/route.ts
+++ src/app/api/lyrics/route.ts
@@ -9,21 +9,3 @@ export async function GET(request: Request) {
     console.error(e);
   }
 }
-
-// import { NextApiRequest, NextApiResponse } from 'next';
-//
-// export default async (req: NextApiRequest, res: NextApiResponse) => {
-//     try {
-//         const client = await clientPromise;
-//         const db = client.db("sample_mflix");
-//         const movies = await db
-//             .collection("movies")
-//             .find({})
-//             .sort({ metacritic: -1 })
-//             .limit(10)
-//             .toArray();
-//         res.json(movies);
-//     } catch (e) {
-//         console.error(e);
-//     }
-// }
