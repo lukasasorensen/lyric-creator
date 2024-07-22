3a0d0de3516b2973856cb06918998a4606bed255
Lukas Sorensen <lsorensen@sportsdigita.com>
2024-07-22 18:28:58 -0500

    cleanup

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,9 +1,5 @@
-import LyricTextEditor from "@/components/LyricTextEditor";
 import LyricEditor from "@/components/Lyrics/LyricEditor";
-import { ThemedButton } from "@/components/Themed";
 import { darkHallowLyrics } from "@/example-data/ExampleLyrics";
-import { ILyrics } from "@/interfaces/Lyrics";
-import { useThemeContext } from "@/providers/ThemeProvider";
 
 export default function Editor() {
   const lyrics = darkHallowLyrics;
