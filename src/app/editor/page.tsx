26272232a43900f4e6fee22d57b235d3d6b15a4f
Lukas Sorensen <lsorensen@sportsdigita.com>
2024-07-23 16:09:17 -0500

    create views folder in components and move lyric editor to it

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -3,10 +3,13 @@ import LyricEditor from "@/components/Lyrics/LyricEditor";
 import { ILyrics } from "@/interfaces/ui/Lyrics";
 
 export default async function Editor() {
-  const allLyrics: ILyrics = await getLyrics();
+  const allLyrics: ILyrics[] = await getLyrics();
   const lyrics = allLyrics?.[0];
 
   return (
-    <main className="lyrics-container">{lyrics && <LyricEditor lyrics={lyrics} />}</main>
+    <main className="lyrics-container">
+      {!lyrics && allLyrics?.length && <></>}
+      {lyrics && <LyricEditor lyrics={lyrics} />}
+    </main>
   );
 }
