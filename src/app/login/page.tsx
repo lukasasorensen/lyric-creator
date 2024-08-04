a4ee36e6fd735b4031be5aed7c1d7e196818ea86
Lukas Sorensen <lsorensen@sportsdigita.com>
2024-08-03 19:52:15 -0500

    remove comment

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -36,14 +36,6 @@ export default function LoginPage() {
 
   return (
     <>
-      {/*
-          This example requires updating your template:
-  
-          ```
-          <html class="h-full bg-white">
-          <body class="h-full">
-          ```
-        */}
       <div className={`flex w-full flex-1 flex-col justify-center px-6 py-12 lg:px-8`}>
         <div className={`sm:mx-auto sm:w-full sm:max-w-sm`}>
           <img
