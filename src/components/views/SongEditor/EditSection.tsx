6d4007e18c60971248848241b0013b7d9a19dd0d
Lukas Sorensen <lsorensen@sportsdigita.com>
2024-08-27 19:13:49 -0500

    dont hide song when saving

--- src/components/views/SongEditor/EditSection.tsx
+++ src/components/views/SongEditor/EditSection.tsx
@@ -104,75 +104,73 @@ export default function EditSection({
 
   return (
     <div className="flex justify-center">
-      {isSaving && <LoadingDisplay text="Saving..." />}
-      {!isSaving && (
-        <div className="container max-w-2xl">
-          {isEditing && (
-            <div className="mb-10">
-              {!!order?.showSectionTitleOnly && (
-                <h3 className="mb-3 mt-5 text-center text-lg font-bold">
-                  {section?.title}
-                  {!!order?.repeatCount && `[x${order.repeatCount}]`}
-                </h3>
-              )}
-              {!order?.showSectionTitleOnly && (
-                <>
-                  <ThemedTextInput
-                    className="text-center"
-                    placeholder="Section Title"
-                    onChange={(e) => {
-                      onSectionTitleChange(section, e.target.value);
-                    }}
-                    value={editTitleText}
-                  />
-                  <textarea
-                    className={`section-input block w-full rounded-md border border-gray-800 p-2.5 text-center leading-10 focus:border-blue-500 focus:ring-blue-500 ${tw.TEXT_PRIMARY} ${tw.BG_PRIMARY}`}
-                    value={editText}
-                    onChange={(e) => onTextChange(section, e)}
-                    ref={inputRef}
-                  ></textarea>
-                </>
-              )}
-              <div className="flex w-full justify-end p-2">
-                <ThemedButton
-                  className=""
-                  text="Delete"
-                  color="danger-secondary"
-                  onClick={() => deleteSection(order.sectionName, index)}
+      <div className="container max-w-2xl">
+        {isSaving && <LoadingDisplay text="Saving..." />}
+        {isEditing && (
+          <div className="mb-10">
+            {!!order?.showSectionTitleOnly && (
+              <h3 className="mb-3 mt-5 text-center text-lg font-bold">
+                {section?.title}
+                {!!order?.repeatCount && `[x${order.repeatCount}]`}
+              </h3>
+            )}
+            {!order?.showSectionTitleOnly && (
+              <>
+                <ThemedTextInput
+                  className="text-center"
+                  placeholder="Section Title"
+                  onChange={(e) => {
+                    onSectionTitleChange(section, e.target.value);
+                  }}
+                  value={editTitleText}
                 />
-                <ThemedButton
-                  className=""
-                  color="primary"
-                  text="Done"
-                  onClick={() => done()}
-                />
-              </div>
+                <textarea
+                  className={`section-input block w-full rounded-md border border-gray-800 p-2.5 text-center leading-10 focus:border-blue-500 focus:ring-blue-500 ${tw.TEXT_PRIMARY} ${tw.BG_PRIMARY}`}
+                  value={editText}
+                  onChange={(e) => onTextChange(section, e)}
+                  ref={inputRef}
+                ></textarea>
+              </>
+            )}
+            <div className="flex w-full justify-end p-2">
+              <ThemedButton
+                className=""
+                text="Delete"
+                color="danger-secondary"
+                onClick={() => deleteSection(order.sectionName, index)}
+              />
+              <ThemedButton
+                className=""
+                color="primary"
+                text="Done"
+                onClick={() => done()}
+              />
             </div>
-          )}
-          {!isEditing && (
-            <div
-              className="container relative rounded-lg p-5 hover:bg-slate-400/30 dark:hover:bg-white/10"
-              onMouseEnter={() => setIsPencilShown(true)}
-              onMouseLeave={() => setIsPencilShown(false)}
-            >
-              {isPencilShown && (
-                <button className="absolute right-5" onClick={() => onEditButtonClick()}>
-                  <FaPencil />
-                </button>
-              )}
-              {!!section && (
-                <Section
-                  edit={true}
-                  section={section}
-                  showSectionTitleOnly={!!order.showSectionTitleOnly}
-                  repeatCount={order?.repeatCount}
-                  onChordChange={onChordChange}
-                />
-              )}
-            </div>
-          )}
-        </div>
-      )}
+          </div>
+        )}
+        {!isEditing && (
+          <div
+            className="container relative rounded-lg p-5 hover:bg-slate-400/30 dark:hover:bg-white/10"
+            onMouseEnter={() => setIsPencilShown(true)}
+            onMouseLeave={() => setIsPencilShown(false)}
+          >
+            {isPencilShown && (
+              <button className="absolute right-5" onClick={() => onEditButtonClick()}>
+                <FaPencil />
+              </button>
+            )}
+            {!!section && (
+              <Section
+                edit={true}
+                section={section}
+                showSectionTitleOnly={!!order.showSectionTitleOnly}
+                repeatCount={order?.repeatCount}
+                onChordChange={onChordChange}
+              />
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
