e0564bc1d84ef73c5ae68a682d28078f3ce5f83f
Lukas Sorensen <lsorensen@sportsdigita.com>
2024-10-13 19:32:13 -0500

    add buttons to the start/end of each line

--- src/components/song/Line.tsx
+++ src/components/song/Line.tsx
@@ -1,5 +1,6 @@
 import { ILine, IWord } from "@/interfaces/db/ISongDb";
 import Word from "@/components/song/Word";
+import { TailWindColorThemeClasses as tw } from "@/constants/ColorTheme";
 
 export default function Line({
   line,
@@ -11,7 +12,11 @@ export default function Line({
   onChordChange?: (word: IWord) => void;
 }) {
   return (
-    <div className="whitespace-pre-line text-center leading-10">
+    <div className="song-line-container whitespace-pre-line text-center leading-10">
+      <div className="add-chord-before-container mr-4 inline-block">
+        <AddChordButton />
+      </div>
+
       {line.words.map((word, i) => (
         <Word
           word={word}
@@ -21,6 +26,22 @@ export default function Line({
           onChordChange={onChordChange}
         />
       ))}
+
+      <div className="add-chord-after-container ml-4 inline-block">
+        <AddChordButton />
+      </div>
+    </div>
+  );
+}
+
+export function AddChordButton() {
+  return (
+    <div className="chord-before-after-container ml-4 inline-block text-center">
+      <button
+        className={`add-chord-after-button ${tw.BTN_PRIMARY} flex flex-col h-5 w-5 justify-center rounded-full`}
+      >
+        +
+      </button>
     </div>
   );
 }
